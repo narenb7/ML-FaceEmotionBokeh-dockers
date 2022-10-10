88ac9f07cd374c7057dee0b6f41fd50a5623d8bd
narenb7 <hinarenb@gmail.com>
2022-10-10 12:24:21 -0400

    Commit

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from starlette.responses import StreamingResponse
 from fastapi import FastAPI, File, UploadFile
 import requests
 
+app = FastAPI()
+face-bokeh = ec2-54-157-42-29.compute-1.amazonaws.com
+face-emotion = ec2-54-157-42-29.compute-1.amazonaws.com
+@app.get("/", tags=["Health Check"])
+async def root():
+#   return {"message": "Service is Online"}
+
+     r1 = requests.get("http://ec2-54-157-42-29.compute-1.amazonaws.com:8001/")
+     r2 = requests.get("http://ec2-54-157-42-29.compute-1.amazonaws.com:8002/")
+     return r1.text, r2.text
+
 #We generate a new FastAPI app in the Prod environment
 #https://fastapi.tiangolo.com/
 
